86c4a14e51069c119a790744b6dbc676886224dc
Devon Campbell <raddevon@gmail.com>
2014-12-04 12:33:47 -0500

    Creates compiled files in dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
 
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['css', 'js', 'fonts']);
 
 gulp.task('css', function() {
   return gulp.src('style.css')
@@ -11,17 +11,24 @@ gulp.task('css', function() {
             }))
             .pipe(plugins.pixrem())
             .pipe(gulp.dest('.'))
+            .pipe(gulp.dest('dist'))
             .pipe(plugins.minifyCss({
               noRebase: true,
               keepSpecialComments: 0
             }))
             .pipe(plugins.rename({extname: '.min.css'}))
-            .pipe(gulp.dest('.'));
+            .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function() {
   return gulp.src('scripts.js')
+            .pipe(gulp.dest('dist'))
             .pipe(plugins.uglify())
             .pipe(plugins.rename({extname: '.min.js'}))
-            .pipe(gulp.dest('.'));
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('fonts/*')
+            .pipe(gulp.dest('dist/fonts'));
 });
